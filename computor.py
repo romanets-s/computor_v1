0fb30a4c487af6a2cd0afcad0136c3211fba8dd8
Serhii Romanets <romanets.sv@gmail.com>
2019-03-28 21:35:09 +0200

    X class and basic function added.

---
+++ computor.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import sys
+
+class X:
+	def __init__(self, digit, degree): #if degree 0 is A, if 1 is B and if 2 is C
+		self.degree = degree
+		self.digit = digit
+	def __str__(self):
+		return str(self.digit) + "X^" + str(self.degree)
+	def __lt__(self, x2): #x < y
+		pass
+	def __le__(self, x2): #x <= y
+		pass
+	def __eq__(self, x2): #x == y
+		pass
+	def __ne__(self, x2): #x != y 
+		pass
+	def __gt__(self, x2): #x > y 
+		pass
+	def __ge__(self, x2): #x >= y 
+		pass
+	def __add__(self, x2):
+		if self.degree == x2.degree:
+			return X(self.digit + x2.digit, self.degree)
+		else:
+			return None
+	def __sub__(self, x2):
+		if self.degree == x2.degree:
+			return X(self.digit - x2.digit, self.degree)
+		else:
+			return None
+	def __mul__(self, x2):
+		if self.degree == x2.degree:
+			return X(self.digit * x2.digit, self.degree)
+		else:
+			return None
+	def __truediv__(self, x2):
+		if self.degree == x2.degree:
+			return X(self.digit / x2.digit, self.degree)
+		else:
+			return None
+
+def parse(input):
+	input = input.replace(' ', '')
+	print(input)
+
+if __name__ == '__main__':
+	if len(sys.argv) == 2:
+		x1 = X(3, 2)
+		x2 = X(5, 2)
+		x3 = X(2, 3)
+		print(x1, x2, x3)
+		print(x1 + x3)
+		print(x1 + x2)
+		print(x1 - x2)
+		print(x1 * x2)
+		print(x1 / x2)
+		parse(sys.argv[1])
+	else:
+		print('not valid input')
